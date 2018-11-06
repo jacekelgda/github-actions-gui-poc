3ff032c3ddf811704886176647fb3852cdadaf25
Jacek Ławniczak <jacekelgda@gmail.com>
2018-11-06 17:27:56 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  drawLine (e) {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    var rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.beginPath();
+    ctx.moveTo(148.5, 5);
+    ctx.lineWidth = 5
+    ctx.lineCap = 'round'
+    ctx.strokeStyle = '#2188ff'
+      ctx.lineTo(x, y);
+      ctx.stroke();
+  }
+
+  componentDidMount () {
+    document.addEventListener("mousemove", this.drawLine);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div className="event">
+            <div className="info">
+              <h4>Some action</h4>
+            </div>
+            <div className="actions">
+              <p>Edit</p>
+            </div>
+            <div className="handle">
+              <div className="handleButton" />
+              <canvas width="300" height="300" id="canvas" />
+            </div>
+          </div>
       </div>
     );
   }
